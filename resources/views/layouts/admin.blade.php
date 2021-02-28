884ed920a139df0f046b9cc2419cfde086f3c87e
Omar Abdo <omar.abdelfatah44@gmail.com>
2021-03-01 01:46:03 +0200

    admin layout

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  <title>{{ config('app.name', 'Tecsee-Test') }}</title>
+
+  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+  <!--     Fonts and icons     -->
+  <link rel="stylesheet" href="{{asset('css/fa/css/all.min.css')}}">
+
+  <!-- CSS Files -->
+  <link href="{{asset('css/material-dashboard.css?v=2.1.0')}}" rel="stylesheet" />
+
+  @stack('css')
+</head>
+
+<body class="">
+  <div class="wrapper ">
+    @include('partials.sidebar')
+    <div class="main-panel">
+      <div class="content">
+        <div class="container-fluid">
+            @yield('content')
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--   Core JS Files   -->
+  <script src="{{asset('js/core/jquery.min.js')}}" type="text/javascript"></script>
+  <script src="{{asset('js/core/popper.min.js')}}" type="text/javascript"></script>
+  <script src="{{asset('js/core/bootstrap-material-design.min.js')}}" type="text/javascript"></script>
+  <script src="{{asset('js/material-dashboard.min.js?v=2.1.0')}}" type="text/javascript"></script>
+  @stack('js')
+</body>
+
+</html>
---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,28 @@
+<div class="sidebar" data-color="purple" data-background-color="white">
+    <div class="logo">
+        <p class="simple-text logo-normal">
+        Tecsee Test
+        </p>
+    </div>
+    <div class="sidebar-wrapper">
+        <ul class="nav nav-pills">
+        <li class="nav-item">
+            <a class="nav-link text-dark {{ Request::segment(1)=="roles" ? "active" : "" }}" data-toggle="collapse" href="#rolesCollapse" role="button" aria-expanded="false" aria-controls="rolesCollapse">
+                <i class="fa fa-list" aria-hidden="true"></i>
+                <p>Roles</p>
+            </a>
+            <div class="collapse {{ Request::segment(1)=="roles" ? "show" : "" }}" id="rolesCollapse">
+                <ul class="nav d-flex px-4">
+                    <li class="nav-item flex-fill {{ Request::is("roles") ? "active" : "" }}">
+                        <a class="nav-link" href="{{route("roles.index")}}">View All Roles</a>
+                    </li>
+                    <li class="nav-item flex-fill {{ Request::is("roles/create") ? "active" : "" }}">
+                        <a class="nav-link" href="{{route("roles.create")}}">Add New Role</a>
+                    </li>
+                </ul>
+            </div>
+ 
+        </li>
+        </ul>
+    </div>
+</div>
